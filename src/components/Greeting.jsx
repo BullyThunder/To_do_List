4bc642dc287f5545476f0f714164f48e29f26e82
Saiko Ruslan <vamosbarca2407@gmail.com>
2024-12-17 10:13:06 +0200

    save

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,17 +1,38 @@
 import React, {useState,useRef} from 'react'
 const Greeting = () => {
-  const ActiveInput = useState(false);
   const[inputValue, setInputValue] = useState('');
   const HandleInputChange =(event) => {
     setInputValue(event.target.value);
   }
-  const[items, setItems] = useState([]);
+  const[items, setItems] = useState(()=>{
+    try{
+    const saved_items = (localStorage.getItem("to_do"));
+    return saved_items ? JSON.parse(saved_items) : [];
+    }
+    catch(e){
+     console.log(`Error${e}`);
+     return [];
+    }
+    });
   const addToList = () =>{
-    setItems([...items,{id:Date.now(),value: inputValue}]);
+    const change_arr = [...items, {id: Date.now(),value:inputValue,checkTemp:false}];
+    setItems(change_arr);
     setInputValue('');
+    saveToLocalStorage(change_arr);
   }
   const deleteFromlist = (id) =>{
-    setItems(items.filter((item) =>item.id !== id));
+  const del_arr = (items.filter((item) =>item.id !== id));
+   setItems(del_arr);
+   saveToLocalStorage(del_arr);
+  }
+  const check_Change = (id,checkTemp) =>{
+    const check_arr = items.map((item)=>
+    item.id === id ? {...item, checkTemp: !item.checkTemp} : item);
+    setItems(check_arr);
+    saveToLocalStorage(check_arr);
+  }
+  const saveToLocalStorage =(data) =>{
+   return localStorage.setItem("to_do",JSON.stringify(data));
   }
   return (
     <div className='header header__container'>
@@ -28,11 +49,17 @@ const Greeting = () => {
       >Add</button>
       </div>
       <div className='header__block'>
-        <ul >
-          {items.map((item)=>(
-          <li key={item.id}>{item.value}  <button onClick={() =>deleteFromlist(item.id)} className="button is-danger is-light">Delete</button></li>
-          ))}
-            
+        <ul>
+          {items.map((item)=>{ 
+         return <li key={item.id}><input
+          type='checkbox'
+          checked={item.checkTemp}
+          onChange={() =>check_Change(item.id)}
+          /> {item.value}  
+          <button 
+           onClick={() =>deleteFromlist(item.id)} 
+          className="button is-danger is-light">Delete</button></li>
+          })}
         </ul>
       </div>
     </div>
